641fc9b077483a38693a0a75d6bf88391df25342
Graziano Statello <grazianos86@gmail.com>
2024-05-16 22:44:13 +0200

    add tests for table and dialog

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -57,11 +57,53 @@ test.describe("table", () => {
     await goToExample(page);
     const table = page.getByTestId("table");
     await expect(table).toBeVisible();
+
+    expect(table.locator(page.getByText("Rome"))).toBeVisible();
   });
   test("get by role", async ({ page }) => {
     await goToExample(page);
     const table = page.getByRole("table", { name: "accessible-table" });
     await expect(table).toBeVisible();
-    await expect(page.getByRole("cell", { name: "Rome" })).toBeVisible();
+    await expect(
+      table.locator(
+        page.getByRole("row").nth(0).locator(page.getByRole("cell").nth(3))
+      )
+    ).toHaveText("Ankara");
+    await expect(
+      page
+        .getByRole("table", { name: "table1" })
+        .locator(
+          page.getByRole("row").nth(0).locator(page.getByRole("cell").nth(3))
+        )
+    ).toHaveText("Ankara");
+  });
+});
+
+test.describe("dialog", () => {
+  test("nope", async ({ page }) => {
+    await goToExample(page);
+    await page.getByRole("button", { name: "Click me", exact: true }).click();
+    const dialog = page.getByTestId("dialog");
+    await expect(dialog).toBeVisible();
+    await expect(
+      dialog.locator(
+        page.getByText(
+          "This is an HTML dialog that opens when you click the button above."
+        )
+      )
+    ).toBeVisible();
+  });
+  test("accessible", async ({ page }) => {
+    await goToExample(page);
+    await page.getByRole("button", { name: "Click me also" }).click();
+    const dialog = page.getByRole("dialog", { name: "fake-dialog" });
+    await expect(dialog).toBeVisible();
+    await expect(
+      dialog.locator(
+        page.getByText(
+          "This is an HTML dialog that opens when you click the button above."
+        )
+      )
+    ).toBeVisible();
   });
 });
